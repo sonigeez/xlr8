1df64e1b69afec42a0caaee926606ffad2ace1cf
Bharat Soni <bharatchills@gmail.com>
2024-02-03 13:43:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# XLR8
+
+- a bare minimum key-value store
+
+TODOs
+
+- [ ] Add a `DELETE` method to the API
+- [ ] Add a `PUT` method to the API
+- [ ] Add CLI support
+- [ ] Build a client
---
+++ app/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var db = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
+type RedisCommand struct {
+	name string
+	args []string
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	for {
+		readLen, err := conn.Read(buffer)
+		if readLen == 0 || err != nil && err.Error() == "EOF" {
+			continue
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			continue
+		}
+		command := parseCommand(string(buffer))
+		switch command.name {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			conn.Write([]byte("+" + command.args[0] + "\r\n"))
+		case "SET":
+			db.Lock()
+			db.m[command.args[0]] = command.args[1]
+			db.Unlock()
+			if len(command.args) > 2 {
+				var timeout time.Duration
+				baseTimeout, _ := strconv.Atoi(command.args[3])
+				if strings.ToUpper(command.args[2]) == "PX" {
+					timeout = time.Duration(baseTimeout) * time.Millisecond
+				} else if strings.ToUpper(command.args[2]) == "EX" {
+					timeout = time.Duration(baseTimeout) * time.Second
+				}
+				timer := time.NewTimer(timeout)
+				go func() {
+					<-timer.C
+					db.Lock()
+					delete(db.m, command.args[0])
+					db.Unlock()
+				}()
+			}
+			conn.Write([]byte("+OK\r\n"))
+		case "GET":
+			db.RLock()
+			if val, ok := db.m[command.args[0]]; ok {
+				conn.Write([]byte("$" + fmt.Sprint(len(val)) + "\r\n" + val + "\r\n"))
+			} else {
+				conn.Write([]byte("$-1\r\n"))
+			}
+			db.RUnlock()
+		}
+	}
+
+}
+
+func parseCommand(command string) RedisCommand {
+	cmd := RedisCommand{}
+	chunks := strings.Split(command, "\r\n")
+	args := make([]string, 0)
+	cmd.name = strings.ToUpper(chunks[2])
+	for i := 4; i < len(chunks); i += 2 {
+		args = append(args, chunks[i])
+	}
+	cmd.args = args
+	return cmd
+}
